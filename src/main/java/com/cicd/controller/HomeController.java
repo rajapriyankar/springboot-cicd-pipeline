36f0368f048c718fd2f6104c303c57237cc09b97
RajapriyankaR <rajapriyanka.r@tech-swing.com>
2026-05-07 17:42:09 +0530

    Feat: Controller

---
+++ src/main/java/com/cicd/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "CI/CD Spring Boot Application Running Successfully";
+    }
+}
